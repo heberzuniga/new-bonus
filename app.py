0a643da4c3af76757d5a3350cd0858c4db4d713e
heberzuniga <heberzuniga@upsa.edu.bo>
2025-08-24 17:19:24 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import streamlit as st
-st.set_page_config(page_title="Smoke test", layout="centered")
-st.title("✅ Smoke Test")
-st.write("Python y dependencias OK")
+import streamlit as st, sys
+st.title("Smoke Test")
+st.write("Python:", sys.version)
